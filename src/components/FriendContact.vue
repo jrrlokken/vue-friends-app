881bcbcaa467f437a49d8d917dab68304e315227
Joshua Lokken <joshualokken@pm.me>
2022-07-05 06:33:06 -0500

    Add favorite functionality

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,55 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsVisible">
+      <li><strong>Age: </strong>{{ age }}</li>
+      <li><strong>Email: </strong>{{ email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ["name", "age", "email", "isFavorite"],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      detailsVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsVisible = !this.detailsVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
+};
+</script>
